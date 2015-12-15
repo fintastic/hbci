082171ff36ffb0e26ce1d87f8bf36d226f029c2f
Roman Lehnert <roman.lehnert@gmail.com>
2015-12-15 16:03:30 +0100

    client fix

--- lib/bankster/hbci/client.rb
+++ lib/bankster/hbci/client.rb
@@ -29,31 +29,12 @@ module Bankster
       end
 
       def balance(account_number)
-        @dialog = Dialog.new(credentials)
-        @dialog.initiate
+        received = balances(account_number)
 
-        messenger = Messenger.new(dialog: @dialog)
+        raise "could not receive balance for account #{account_number}" if received[account_number].nil?
 
-        balance_request_segment = Segments::HKSALv4.build(dialog: @dialog)
-        balance_request_segment.account.code = @dialog.credentials.bank_code
-        balance_request_segment.account.number = account_number
-        balance_request_segment.all_accounts = "N"
-
-        messenger.add_request_payload(balance_request_segment)
-        messenger.request!
-
-        balances = messenger.response.payload.select { |seg| 
-          seg.head.type == "HISAL" 
-        }.each_with_object({}) { |seg, output|
-          output[seg.ktv.number] = seg.booked_amount
-        }
-        if balances[account_number].nil?
-          raise "could not receive balance for account #{account_number}"
-        else
-          balances[account_number]
-        end
+        received[account_number]
       end
-
     end
   end
 end
