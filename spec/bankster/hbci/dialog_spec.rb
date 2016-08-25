38788408d74d03d246ed50796a41b700024ecfbd
Roman Lehnert <roman.lehnert@gmail.com>
2016-08-25 12:24:53 +0200

    Spec for dialog inititiation

--- spec/bankster/hbci/dialog_spec.rb
+++ spec/bankster/hbci/dialog_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 
 describe Bankster::Hbci::Dialog do
   describe 'initiate' do
-    let(:credentials) do
-      Bankster::BankCredentials::Hbci.new(url:        'https://hbci11.fiducia.de/cgi-bin/hbciservlet',
-                                          bank_code:  '0000',
-                                          user_id:    '1111',
-                                          pin:        'pin')
-    end
+    let(:credentials)            { build(:hbci_credentials) }
+    let!(:dialog_init_request)   { stub_dialog_init_request(credentials) }
 
     subject { described_class.new(credentials) }
+
+    before do
+      Timecop.freeze
+      allow(Bankster::Hbci::Message).to receive(:generate_security_reference).and_return('10999990')
+    end
+
+    it 'initiates the dialog' do
+      expect(subject).to_not be_initiated
+
+      subject.initiate
+
+      expect(dialog_init_request).to have_been_made.once
+      expect(subject).to be_initiated
+    end
   end
 end
