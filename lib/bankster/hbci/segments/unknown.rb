f67127505b925450296f4428ee5d639ba32cb6d5
Roman Lehnert <roman.lehnert@gmail.com>
2016-08-25 08:54:35 +0200

    Fix segment filling for ruby 2.3.1

--- lib/bankster/hbci/segments/unknown.rb
+++ lib/bankster/hbci/segments/unknown.rb
@@ -4,6 +4,10 @@ module Bankster
       class Unknown < Segment
         element_group :head, type: ElementGroups::SegmentHead
 
+        10.times do |i|
+          element_group "element_group_#{i}".to_sym, type: ElementGroups::Unknown
+        end
+
         def self.type
           'Unknown'
         end
