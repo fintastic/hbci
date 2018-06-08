edcbc6dd6d2989a06d6c90345319dc48bfd567e8
Roman Lehnert <roman.lehnert@gmail.com>
2018-06-08 13:39:36 +0200

    Fix typo in transaction example

--- examples/get_transactions.rb
+++ examples/get_transactions.rb
@@ -4,6 +4,6 @@ require_relative 'credentials'
 start_date = 1.month.ago
 end_date = Time.now
 
-@client.transactions(@acount_number, start_date, end_date).each do |transaction|
+@client.transactions(@account_number, start_date, end_date).each do |transaction|
   puts transaction
 end
