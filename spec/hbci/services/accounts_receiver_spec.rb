7c95ec9e118f2762610ac31f4530d0ac0d803562
Roman Lehnert <roman.lehnert@gmail.com>
2018-07-02 10:28:23 +0200

    Apply rubocops default Layout/IndentArray settings

--- spec/hbci/services/accounts_receiver_spec.rb
+++ spec/hbci/services/accounts_receiver_spec.rb
@@ -25,13 +25,13 @@ describe Hbci::Services::AccountsReceiver do
 
   it 'requests and returns the balance' do
     expect(subject.perform).to eql([
-      { account_number: '11111111', bank_code: '74090000' },
-      { account_number: '22222222', bank_code: '74090000' },
-      { account_number: '333333333', bank_code: '74090000' },
-      { account_number: '444444444', bank_code: '74090000' },
-      { account_number: '5555555555', bank_code: '74090000' },
-      { account_number: '6666666666', bank_code: '74090000' },
-      { account_number: '7777777777', bank_code: '74090000' }
-    ])
+                                     { account_number: '11111111', bank_code: '74090000' },
+                                     { account_number: '22222222', bank_code: '74090000' },
+                                     { account_number: '333333333', bank_code: '74090000' },
+                                     { account_number: '444444444', bank_code: '74090000' },
+                                     { account_number: '5555555555', bank_code: '74090000' },
+                                     { account_number: '6666666666', bank_code: '74090000' },
+                                     { account_number: '7777777777', bank_code: '74090000' }
+                                   ])
   end
 end
