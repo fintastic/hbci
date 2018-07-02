7d2e289a91baa5c87e78e70d8cf37c807ac5a4e4
Roman Lehnert <roman.lehnert@gmail.com>
2018-07-02 10:39:22 +0200

    Add frozen string literal comment to request stubs

--- spec/support/request_stubs.rb
+++ spec/support/request_stubs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def stub_dialog_init_request(credentials)
   stub_request(:post, credentials.url)
     .with(body: Base64.encode64(stub_dialog_init_request_message(credentials)))
